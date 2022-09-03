5799169b0078b41de65b9d122777f5693111d2a6
[Ruliaga] <ruliaga@mail.ru>
2022-09-04 01:31:31 +0500

    Task34 done

---
+++ Task34/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Нажмите Enter для вывода массива.");
+Console.ReadLine();
+
+Random random = new Random();
+
+int[] array = new int[random.Next(3,10)];
+
+Console.Write("[");
+
+int result=0;
+
+for (int i=0;i<array.Length;i++){
+    array[i]=random.Next(100,999); 
+
+    if ((array[i]%2)==0){result=result+1;}
+
+    Console.Write(array[i]);
+    if (i<array.Length-1){
+        Console.Write(", ");
+    } else {
+        Console.Write("]");}
+
+}
+
+Console.Write($" -> {result}");
